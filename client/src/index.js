79935168f40f6419208782a92a80b18381897fab
HiIamJeff67 <iamjeffhi67@gmail.com>
2024-02-11 14:06:09 +0800

    v2.0.6

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/FHIB-Website'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
